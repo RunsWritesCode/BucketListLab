e9e86244809fea2c32e3f2bb4a8ba654d5987f0e
RunsWritesCode <sophieamullins@gmail.com>
2018-02-07 14:49:11 +0000

    data persisting to db

--- server.js
+++ server.js
@@ -1,8 +1,46 @@
-var express = require('express');
-var server = express();
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
+const express = require('express');
+const server = express();
+
+server.use(parser.json());
 server.use(express.static('client/build'));
+server.use(parser.urlencoded({extended: true}));
 
-server.listen(3000, function() {
-  console.log('app is listening on port 3000');
-})
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+  if (err) {console.log(err); return;}
+  const db = client.db('countries');
+  console.log('connected to db');
+
+
+  server.post('/api/countries', function(req, res) {
+    db.collection('countries').save(req.body, function(err, result) {
+      if(err){console.log(err);
+        res.status(500);
+        res.send();
+      }
+      res.status(201);
+      res.json(result.ops[0]);
+      console.log('saved to db');
+    });
+  });
+
+  server.get('/api/countries', function(req, res) {
+    db.collection('countries').find().toArray(function (err, result) {
+      if(err){console.log(err);
+        res.status(500);
+        res.send();
+      }
+      res.status(200);
+      res.json(result);
+      console.log('all items returned');
+    })
+  })
+
+
+  server.listen(3000, function() {
+    console.log('app is listening on port 3000');
+  })
+
+});
